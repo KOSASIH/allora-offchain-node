74a47a187c2c0b132b23a7b36e646593b6f4932d
Diego C <xmariachi@gmail.com>
2025-05-14 23:25:20 +0200

    Add error-level metrics, remove unused metrics (#130)
    <!-- < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < ☺
    v           ✰  Thanks for creating a PR! You're awesome! ✰
    v Please note that maintainers will only review those PRs with a
    completed PR template.
    ☺ > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >  -->
    
    ## Purpose of Changes and their Description
    * Add error metrics
    * Remove unused metrics
    * Fix handling of `worker/reputer nonce window not available` errors if
    coming without error code from chain.
    
    ## Are these changes tested and documented?
    
    - [x] If tested, please describe how. If not, why tests are not needed.
    -- tested against a local chain
    - [x] If documented, please describe where. If not, describe why docs
    are not needed. --
    - [x] Added to `Unreleased` section of `CHANGELOG.md`?

--- metrics/constant.go
+++ metrics/constant.go
@@ -3,14 +3,13 @@ package metrics
 // Default labels used by most counters
 var DefaultLabels = []string{"address", "topic"}
 var EndpointLabels = []string{"endpoint"}
+var ErrorCodeLabels = []string{"address", "error_code"}
 
 // metrics
 const (
 	InferenceRequestCount          string = "allora_worker_inference_request_count"
 	ForecastRequestCount           string = "allora_worker_forecast_request_count"
 	TruthRequestCount              string = "allora_reputer_truth_request_count"
-	WorkerDataBuildCount           string = "allora_worker_data_build_count"
-	ReputerDataBuildCount          string = "allora_reputer_data_build_count"
 	WorkerChainSubmissionCount     string = "allora_worker_chain_submission_count"
 	ReputerChainSubmissionCount    string = "allora_reputer_chain_submission_count"
 	WorkerProcessFinishedCount     string = "allora_worker_process_finished_count"
@@ -19,6 +18,7 @@ const (
 	GRPCConnectionLostCount        string = "allora_grpc_connection_lost_count"
 	GRPCReconnectionCount          string = "allora_grpc_reconnection_count"
 	GRPCConnectionPermanentFailure string = "allora_grpc_connection_permanent_failure"
+	ActorTxErrorCount              string = "allora_actor_tx_error_count"
 )
 
 // A struct that holds the name and help text for a prometheus counter
@@ -26,8 +26,6 @@ var CounterData = []MetricsCounter{
 	{InferenceRequestCount, "The total number of times worker requests inference from source", DefaultLabels},
 	{ForecastRequestCount, "The total number of times worker requests forecast from source", DefaultLabels},
 	{TruthRequestCount, "The total number of times reputer requests truth from source", DefaultLabels},
-	{WorkerDataBuildCount, "The total number of times worker built data successfully", DefaultLabels},
-	{ReputerDataBuildCount, "The total number of times worker built data successfully", DefaultLabels},
 	{WorkerChainSubmissionCount, "The total number of worker commits to the chain", DefaultLabels},
 	{ReputerChainSubmissionCount, "The total number of reputer commits to the chain", DefaultLabels},
 	{WorkerProcessFinishedCount, "The total number of worker processes finished", DefaultLabels},
@@ -36,4 +34,5 @@ var CounterData = []MetricsCounter{
 	{GRPCConnectionLostCount, "The total number of times the GRPC connection is lost", EndpointLabels},
 	{GRPCReconnectionCount, "The total number of times the GRPC connection is successfully reconnected", EndpointLabels},
 	{GRPCConnectionPermanentFailure, "The total number of times the GRPC connection is lost", EndpointLabels},
+	{ActorTxErrorCount, "The total number of errors by error code", ErrorCodeLabels},
 }
